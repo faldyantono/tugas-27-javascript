e5e47ce4dbbdbb56651c89e7e9f8f81baf856958
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 15:12:35 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,8 @@
+var a = ["No", "Rumah Saya", 30, "A"];
+var b = ["Jalan pangeran Tirtayasa No", 70];
+var c = a.join(" ");
+var d = b.join(" ")
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
